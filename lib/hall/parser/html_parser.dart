676b8cf8467ef188202c2c72c85c69bb8a308423
csj5588 <1839333350@qq.com>
2021-12-07 17:30:28 +0800

    feat: pageFrame template development progress 50%

--- lib/hall/parser/html_parser.dart
+++ lib/hall/parser/html_parser.dart
@@ -3,16 +3,13 @@
 /// pageFrame模版模块必备方法
 /// 如果想体验可以放在hall_page入口进行体验
 
-import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutterminiprogram/jsBridge/jsBridgeEntity.dart';
 import 'package:flutterminiprogram/utils/javascriptChannel.dart';
+import 'package:flutterminiprogram/utils/server.dart';
 import 'package:flutterminiprogram/utils/webview.dart';
-import 'package:http_server/http_server.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-import 'package:html/dom.dart' as DOM;
 import 'package:html/parser.dart' show parse, parseFragment;
 
 class HtmlParser extends StatefulWidget {
@@ -23,101 +20,62 @@ class HtmlParser extends StatefulWidget {
 }
 
 class HtmlParserState extends State<HtmlParser> {
-  late HttpServer server;
   /// channel
   late JavascriptChannel channel;
   /// webview controller
   late WebViewController _webViewController;
+  List history = [];
 
   @override
   void initState() {
-    _initHttpServer();
     _initChannel();
+
+    _createNewServer([
+      ServerSource(path: '/', rootPath: 'miniprogram/pageFrame/pageFrame.html'),
+      ServerSource(path: '/WAWebview.js', rootPath: 'miniprogram/pageFrame/WAWebview.js', header: ServerSourceHeader(contentType: 'text/javascript; charset=utf-8')),
+      ServerSource(path: '/wxml.js', rootPath: 'miniprogram/pageFrame/wxml.js', header: ServerSourceHeader(contentType: 'text/javascript; charset=utf-8'))
+    ]);
 
     super.initState();
   }
 
   @override
   void dispose() {
-    this.server.close();
-
+    FmServer.closeAllServer();
     super.dispose();
   }
 
-  void _initHttpServer() async {
-    HttpServer.bind('0.0.0.0', 8000).then((server) {
-      this.server = server;
-
-      print('Server running at: ${server.address.address}:${server.address.host}');
-      server.transform(HttpBodyHandler()).listen((HttpRequestBody body) async {
-
-      /// [request] uri
-      print('Request URI'); 
-
-      /// [response] rule
-      switch (body.request.uri.toString()) {
-        case '/':
-          {
-            String filePath = 'miniprogram/parser.html';
-            String fileHtmlContents = await rootBundle.loadString(filePath);
-
-            /// [analyze html]💥
-            /// 解析HTML为document树，满足基本增删查改需求
-            /// 后期pageFrame模板生成新页面的时候可以使用此方法进行节点插入，修改。
-            /// 两种方式
-            /// [1] 通过document类进行操作
-            /// [2] 通过replace方法替换指定html备注（例如: <!-- remark -->）
-            DOM.Document document = parse(fileHtmlContents);
-
-            // 创建generateFuncReady监听
-            String generateFuncReadyScript = '''
-              <script>
-                (function() {
-                  if (document.readyState === 'complete') {
-                    Native.postMessage(JSON.stringify({
-                      method: 'DOCUMENT_READY',
-                    }))
-                  } else {
-                    const fn = () => {
-                      Native.postMessage(JSON.stringify({
-                        method: 'DOCUMENT_READY',
-                      }))
-                      window.removeEventListener('load', fn)
-                    }
-                    window.addEventListener('load', fn)
-                  }
-                })()
-            ''';
-
-            document.body?.nodes.add(parseFragment(generateFuncReadyScript));
-
-            String changeFileContents = document.outerHtml;
-
-            body.request.response.statusCode = 200;
-            body.request.response.headers.set("Content-Type", "text/html; charset=utf-8");
-            body.request.response.write(changeFileContents);
-            body.request.response.close();
-            break;
-          }
-        default: {
-          body.request.response.statusCode = 404;
-          body.request.response.write('Not found');
-          body.request.response.close();
-        }
-      }});
-    });
+  /// 创建一个新端口并讲地址推入history
+  void _createNewServer(List<ServerSource> sourceList) {
+    FmServer.createServer(
+      sourceList: sourceList,
+      onSuccess: (HttpServer server) {
+        history.add('http://localhost:${server.port}');
+        setState(() {
+          history = history;
+        });
+      }
+    );
   }
 
   void _initChannel() {
     /// 创建channel
     channel = JavascriptChannelSingle.createChannel(
       onMessageReceived: (JsBridge bridge) {
-        /// [bridge] DOCUMENT_READY
-        /// 传递动态数据
-        String commend = 'window.exparser.createVirtualNode()';
-        Future.delayed(Duration(milliseconds: 300), () {
-          _webViewController.evaluateJavascript(commend);
-        });
+
+        print('log:--------bridge.method:${bridge.method}');
+
+        if (bridge.method == 'DOCUMENT_READY') {
+          /// [bridge] DOCUMENT_READY
+          /// 获取初始动态数据
+          /// 插入wxml.js等
+          String wxml = './wxml.js';
+          String scriptType = 'text/javascript';
+          String commend = 'window.foundtion.insertDocumentHeadChild("$wxml", "$scriptType")';
+          Future.delayed(Duration(milliseconds: 300), () {
+            _webViewController.evaluateJavascript(commend);
+          });
+        }
       }
     );
   }
@@ -126,14 +84,29 @@ class HtmlParserState extends State<HtmlParser> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Server Page'),
+        title: Text('Parser Page'),
       ),
-      body: FmWebview(
-        initialUrl: 'http://localhost:8000',
-        onWebviewChange: (WebViewController webviewController) {
-          _webViewController = webviewController;
-        },
-        channel: channel,
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: Stack(
+          children: history.map((address) {
+            return Align(
+              alignment: Alignment.topCenter,
+              child: Container(
+                width: double.infinity,
+                height: double.infinity,
+                child: FmWebview(
+                  initialUrl: address,
+                  onWebviewChange: (WebViewController webviewController) {
+                    _webViewController = webviewController;
+                  },
+                  channel: channel,
+                ),
+              ),
+            );
+          }).toList(),
+        ),
       )
     );
   }
---
+++ miniprogram/pageFrame/WAWebview.js
@@ -0,0 +1,150 @@
+(function() {
+  window.exparser = {
+    /**
+     * 会有很多的自定义组件初始化完毕。
+     * 都是一个一个小的shadow-dom
+     */
+    components: {},
+    register: function(e) {
+      // 创建root
+      var element = document.createElement(e.is); // wx-button
+      // 转化shadow-DOM
+      var shadow = element.attachShadow({mode: 'open'});
+      // 编译template - loop
+      function loopParser(children, root) {
+        children.forEach(function(child) {
+          var childDOM = document.createElement(child.name);
+          // 添加样式
+          Object.keys(child.style).forEach(function(key) {
+            childDOM.style[key] = child.style[key];
+          });
+  
+          // 如果有文案
+          if (child.data) {
+            childDOM.innerText = child.data;
+          }
+  
+          // loop判定
+          if (child.children) {
+            loopParser(child.children, childDOM);
+          }
+  
+          // 添加元素
+          root.appendChild(childDOM);
+        });
+      }
+  
+      loopParser(e.template(), shadow);
+
+      /// 这里应该挂载一个生成组件函数。因为组件也需要接收动态参数。
+      this.components[e.is] = element;
+    },
+    createVirtualNode: function(data) {
+      // 这里应该通过generateFunc获取
+      var virtualNode = {
+        tag: 'wx-button',
+        children: '这是按钮默认文案'
+      }
+  
+      // loop-virtualDOM
+      const getVirtualDOM = (root) => {
+        var element = this.components[root.tag];
+        return element;
+      }
+  
+      var virtualDOM = getVirtualDOM(virtualNode);
+  
+      document.body.appendChild(virtualDOM);
+    }
+  }
+  
+  /**
+   * 向组件库中注册组件
+   * 注册的组件会存储在注册表(exparser.components)中
+   */
+  window.exparser.register({
+    is: 'wx-button',
+    template: function() {
+      return [{
+        name: 'div',
+        style: {
+          padding: '20px',
+          background: '#1890ff',
+          textAlign: 'center',
+          borderRadius: '10px',
+        },
+        children: [{
+          name: 'span',
+          style: {
+            color: '#ffffff'
+          },
+          data: '默认文案'
+        }]
+      }]
+    }
+  })
+
+  window.foundtion = {
+    /**
+     * @desc 向文档head中插入child方法
+     */
+    insertDocumentHeadChild(src, type) {
+      /**
+       * native log
+       */
+      native.invoke(`insertDocumentHeadChild load! src: ${src} 、 type:${type}`);
+
+      /**
+       * 创建业务script并插入
+       * 后面改为批量模式
+       */
+      const scriptNode = document.createElement('script');
+      scriptNode.type = type || 'text/javascript';
+      scriptNode.src = src;
+      document.head.appendChild(scriptNode);
+
+      this.addGenerateFuncReady();
+
+      this.replaceDocumentReadyScript();
+    },
+    /**
+     * @desc 添加generateFuncReady CE
+     */
+    addGenerateFuncReady() {
+      const scriptNode = document.createElement('script');
+      scriptNode.innerHTML = `
+        var decodeName = decodeURI('index.wxml');
+        var generateFunc = $gwx(decodeName);
+
+        if (generateFunc) {
+          var CE = (typeof __global === 'object') ? (window.CustomEvent || __global.CustomEvent) : window.CustomEvent;
+          document.dispatchEvent(new CE("generateFuncReady", {
+            detail: {
+              generateFunc: generateFunc,
+            }
+          }))
+        }
+      `;
+      /**
+       * @desc 这里插入的时候获取不到$gwx,这个问题需要排查
+       */
+      setTimeout(() => {
+        document.head.appendChild(scriptNode);
+      }, 200)
+    },
+    replaceDocumentReadyScript() {
+      var scripts = document.getElementsByTagName('script');
+      var noticeScript = scripts[scripts.length - 1];
+
+      noticeScript.parentNode.removeChild(noticeScript);
+    }
+  }
+
+  window.native = {
+    invoke: function(method) {
+      Native.postMessage(JSON.stringify({
+        method: method,
+      }))
+    }
+  }
+})()
